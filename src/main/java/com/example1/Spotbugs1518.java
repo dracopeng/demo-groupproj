c73aa66779684000153175dcdeb737d72dba785a
Xin Peng <xinp2@illinois.edu>
2021-11-20 19:30:23 -0600

    init status before fixing the test error

--- src/main/java/com/example1/Spotbugs1518.java
+++ src/main/java/com/example1/Spotbugs1518.java
@@ -5,7 +5,6 @@ import java.util.Random;
 public class Spotbugs1518 {
     void test(){
         Random random = new Random();
-        float num = 1;
         float f = random.nextFloat() * num; // no warning (false negative)
     }
 }
