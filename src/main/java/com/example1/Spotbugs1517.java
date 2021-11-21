d2111bb0488fb9387d26f30f186097aba5f025f0
Xin Peng <xinp2@illinois.edu>
2021-11-21 00:18:59 -0600

    updated

--- src/main/java/com/example1/Spotbugs1517.java
+++ src/main/java/com/example1/Spotbugs1517.java
@@ -3,7 +3,7 @@ package com.example1;
 public class Spotbugs1517 {
     void test00(){
         int i = 0;
-        i = 2 + (i++); // no warning (false negative)'
-        // i = 2;
+        // i = 2 + (i++); // no warning (false negative)'
+        i = 2;
     }
 }
